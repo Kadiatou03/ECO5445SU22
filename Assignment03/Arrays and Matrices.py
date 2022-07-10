8c6d57462cba3f3d67710996cdce85b6049841e7
Kadiatou03 <108303138+Kadiatou03@users.noreply.github.com>
2022-07-10 15:09:12 -0400

    Create Arrays and Matrices.py

---
+++ Assignment03/Arrays and Matrices.py
@@ -0,0 +1,7 @@
+import os
+
+import numpy as np
+
+#Constructing the following array "A" with i = 3 rows and j = 4 columns: 
+
+A = np.array([[1., 2., 3., 4.], [5., 6., 7., 8.], [9., 10., 11., 12.]])
