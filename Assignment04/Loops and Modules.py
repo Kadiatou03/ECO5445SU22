e97230b71df01dccd2fd5874c510b0ec7c7a410b
Kadiatou03 <108303138+Kadiatou03@users.noreply.github.com>
2022-07-10 23:49:02 -0400

    Loops and Modules

--- Assignment04/Loops and Modules.py
+++ Assignment04/Loops and Modules.py
@@ -1,27 +1,23 @@
 import numpy as np 
 
-# Set the number of points that would be generated
+# Set the number of points that would be generated as numPoints and the number of points in the circle as pointsInCercle equals to zero to start
 numPoints = 10000
 pointsInCircle = 0 
 
-# Used np.random.rand() function to help me order data in a way what it is easier to manipulate 
-# and to organize data into array of points with coordinates (x,y)
-p = np.random. rand (numPoints,2) 
+# Use np.random.rand() function to help order data in a way what it is easier to manipulate and to organize data into array of points with coordinates (x,y)
+p = np.random.rand(numPoints,2) 
 print(p) 
 
-# Looping through the array of points, where point is represented as array of 2 values, 
-# essentially x and y coordinates, then store first value as x and second value as y
+# Loop through the array of points, where point is represented as array of 2 values, essentially x and y coordinates, then store first value as x and second value as y
 # to compute the distance
-
 for i in range(len(p)):
     x = p[i, 0]
     y = p[i, 1] 
     print("x: ", x) 
     print("y: ", y) 
     
-# Distance is found as unit square circle where radius = 1, 
-# therefore distance from the point to origin has to be <= 1, 
-# if that is true, the point is in the circle and we increase count (pointsInCircle)
+# Distance is found by doing the squared root of (x*x + y*y) and as unit square circle where radius = 1, therefore distance from the point to origin has to be <= 1,
+# if that is true, the point is in the circle and that increases the count of "pointsInCircle"
  
     distance = np.sqrt(x*x + y*y) 
     print("distance: ", distance) 
@@ -29,7 +25,7 @@ for i in range(len(p)):
     if distance <= 1: 
      pointsInCircle += 1 
      
-# Wrote the pi formula and print our pi estimation     
+# Wrote the pi formula and print the pi estimation     
      
 pi = 4 * (pointsInCircle / numPoints) 
 print("pi: ", pi)
