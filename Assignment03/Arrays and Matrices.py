15a80b0eb5f98dadc58eb2b3e3a37b49615e0bf5
Kadiatou03 <108303138+Kadiatou03@users.noreply.github.com>
2022-07-10 23:26:32 -0400

    Arrays and Matrices

--- Assignment03/Arrays and Matrices.py
+++ Assignment03/Arrays and Matrices.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-#Constructing the following array "A" with i = 3 rows and j = 4 columns: 
+# Constructing array "A" with i = 3 rows and j = 4 columns: 
 
 A = np.array([[1., 2., 3., 4.], [5., 6., 7., 8.], [9., 10., 11., 12.]])
 print("A =", A)
@@ -15,29 +15,27 @@ print("columns")
 
 
 
-#From array A, extract "number 7" 
-
+# From array A, extract "number 7" 
 A[1][2]
 
-#From array A,"row 1"
+# From array A,"row 1"
 A[0]
 
-#From array A,"column 2"
+# From array A,"column 2"
 A[: ,1]
 
-#From array A,"rows 2 and 3" 
+# From array A,"rows 2 and 3" 
 A[1], A[2]
 
-#From array A, "values 7, 8, 11, and 12"
+# From array A, "values 7, 8, 11, and 12"
 A[1][2], A[1][3], A[2][2], A[2][3]
 
 
 
 
-#Create the array B = 2*A - 8. 
+# Create the array B = 2*A - 8. 
 B = 2*A - 8.
 print('B =', B)
-
 
 # The sum of the row values
 np.sum(B, axis=1) 
@@ -59,25 +57,24 @@ print("Sum fourth column =", sum(B [: ,3]))
 # The cumulative sum of row values
 np.cumsum(B, axis=1) 
 
-#the cumulative sum of column values
+# The cumulative sum of column values
 np.cumsum(B, axis=0) 
 
 
 
-#From array B, create new arrays containing the element-by-element: natural logarithm
+# From array B, create new arrays containing the element-by-element: natural logarithm
 l = np.log(B)
-print(l)
-      
+print(l)     
 
-#From array B, create new arrays containing the element-by-element: square root
-sr = (np.sqrt(B))
+# From array B, create new arrays containing the element-by-element: square root
+sr = np.sqrt(B)
 print(sr)
 
-#From array B, create new arrays containing the element-by-element: square
-sq = (np.square(B))
+# From array B, create new arrays containing the element-by-element: square
+sq = np.square(B)
 print(sq)
 
-#From array B, create new arrays containing the element-by-element: absolute value
+# From array B, create new arrays containing the element-by-element: absolute value
 ab = np.abs(B)
 print(ab)
 
@@ -101,11 +98,11 @@ w = np.matrix([[286], [88]])
 v_inverse = np.linalg.inv(v)
 print(v_inverse)
 
-#Solving final answer 's'
+# Solving for final answer 's'
 s = v_inverse * w
 print(s)
 
-# The equilibrium price is 3.3 and equilibrium quantity is 220.
+# Answer: The equilibrium price is 3.3 and equilibrium quantity is 220.
 
 
 
